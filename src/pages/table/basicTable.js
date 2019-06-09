daf895008131056e66d39cd2541d2d5d17858878
hellozjf <908686171@qq.com>
2019-06-10 01:27:09 +0800

    封装axios插件请求数据，实现动态表格

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,46 +1,68 @@
 import React from 'react'
 import {Card, Table} from "antd";
+import axios from './../../axios/index'
 
 export default class FormLogin extends React.Component {
 
-  state = {}
+  state = {
+    dataSource2: []
+  }
 
   componentDidMount() {
     const dataSource = [
       {
-        id:'0',
-        userName:'Jack',
-        sex:'1',
-        state:'1',
-        interest:'1',
-        birthday:'2000-01-01',
-        address:'北京市海淀区奥林匹克公园',
-        time:'09:00'
+        id: '0',
+        userName: 'Jack',
+        sex: '1',
+        state: '1',
+        interest: '1',
+        birthday: '2000-01-01',
+        address: '北京市海淀区奥林匹克公园',
+        time: '09:00'
       },
       {
-        id:'1',
-        userName:'Tom',
-        sex:'1',
-        state:'1',
-        interest:'1',
-        birthday:'2000-01-01',
-        address:'北京市海淀区奥林匹克公园',
-        time:'09:00'
+        id: '1',
+        userName: 'Tom',
+        sex: '1',
+        state: '1',
+        interest: '1',
+        birthday: '2000-01-01',
+        address: '北京市海淀区奥林匹克公园',
+        time: '09:00'
       },
       {
-        id:'2',
-        userName:'Lily',
-        sex:'1',
-        state:'1',
-        interest:'1',
-        birthday:'2000-01-01',
-        address:'北京市海淀区奥林匹克公园',
-        time:'09:00'
+        id: '2',
+        userName: 'Lily',
+        sex: '1',
+        state: '1',
+        interest: '1',
+        birthday: '2000-01-01',
+        address: '北京市海淀区奥林匹克公园',
+        time: '09:00'
       }
     ]
     this.setState({
       dataSource: dataSource
     })
+    this.request();
+  }
+
+  // 动态获取mock数据
+  request = () => {
+    axios.ajax({
+      url: '/table/list1',
+      data: {
+        params: {
+          page: 1
+        }
+      }
+    }).then((res) => {
+      if (res.code == 0) {
+        this.setState({
+          dataSource2: res.data
+        })
+      }
+    });
   }
 
   render() {
@@ -85,6 +107,14 @@ export default class FormLogin extends React.Component {
             bordered
             columns={columns}
             dataSource={this.state.dataSource}
+            pagination={false}
+          />
+        </Card>
+        <Card title="动态数据渲染表格" style={{margin: '10px 0'}}>
+          <Table
+            bordered
+            columns={columns}
+            dataSource={this.state.dataSource2}
             pagination={false}
           />
         </Card>
