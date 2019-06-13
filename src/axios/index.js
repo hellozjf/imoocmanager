2ec11af27f1e959833122b57c35689e8f03aac1a
hellozjf <908686171@qq.com>
2019-06-14 00:06:20 +0800

    update

--- src/axios/index.js
+++ src/axios/index.js
@@ -23,7 +23,8 @@ export default class Axios {
       loading = document.getElementById('ajaxLoading');
       loading.style.display = 'block';
     }
-    let baseApi = 'http://localhost:8080/mockapi';
+    // let baseApi = 'http://localhost:8080/mockapi';
+    let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
     return new Promise((resolve,reject)=>{
       axios({
         url:options.url,
--- src/pages/table/highTable.js
+++ src/pages/table/highTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, Table, Modal, Button, message} from "antd";
+import {Card, Table, Modal, Button, message, Badge} from "antd";
 import axios from './../../axios/index'
 import Utils from "../../utils/utils";
 
@@ -39,8 +39,20 @@ export default class FormLogin extends React.Component {
   handleChange = (pagination, filters, sorter)=>{
     console.log("::" + sorter)
     this.setState({
-      sortOrder: sorter
+      sortOrder: sorter.order
     })
+  }
+
+  handleDelete = (item) => {
+    let id = item.id;
+    Modal.confirm({
+      title:'确认',
+      content:'您确认要删除此条数据吗？',
+      onOk:()=>{
+        message.success('删除成功');
+        this.request();
+      }
+    });
   }
 
   render() {
@@ -395,6 +407,69 @@ export default class FormLogin extends React.Component {
         dataIndex: 'time'
       },
     ];
+    const columns4 = [
+      {
+        title: 'id',
+        dataIndex: 'id'
+      },
+      {
+        title: '用户名',
+        dataIndex: 'userName'
+      },
+      {
+        title: '性别',
+        dataIndex: 'sex',
+        render(sex) {
+          return sex == 1 ? '男' : '女'
+        }
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+      },
+      {
+        title: '状态',
+        dataIndex: 'state',
+        render(state) {
+          let config = {
+            '1': '咸鱼一条',
+            '2': '风华浪子',
+            '3': '北大才子',
+            '4': '百度FE',
+            '5': '创业者'
+          };
+          return config[state];
+        }
+      },
+      {
+        title: '爱好',
+        dataIndex: 'interest',
+        render(abc) {
+          let config = {
+            '1': <Badge status="success" text="成功"/>,
+            '2': <Badge status="error" text="报错"/>,
+            '3': <Badge status="default" text="正常"/>,
+            '4': <Badge status="processing" text="进行中"/>,
+            '5': <Badge status="warning" text="警告"/>,
+          }
+          return config[abc]
+        }
+      },
+      {
+        title: '生日',
+        dataIndex: 'birthday'
+      },
+      {
+        title: '地址',
+        dataIndex: 'address'
+      },
+      {
+        title: '操作',
+        render: (text, item) => {
+          return <Button size="small" onClick={(item)=>{ this.handleDelete(item)} }>删除</Button>
+        }
+      },
+    ];
 
     // 累加出x轴滚动所需要的宽度
     let totalWidth = 0;
@@ -432,6 +507,14 @@ export default class FormLogin extends React.Component {
             dataSource={this.state.dataSource}
             pagination={false}
             onChange={this.handleChange}
+          />
+        </Card>
+        <Card title="操作按钮" style={{margin: '10px 0'}}>
+          <Table
+            bordered
+            columns={columns4}
+            dataSource={this.state.dataSource}
+            pagination={false}
           />
         </Card>
       </div>
